ca949cf1442693230f461db28f27f3bd642fb47f
Amir Vafaei <amir.vafaei@vineti.com>
2021-02-08 21:16:02 +0400

    continued working on the bomb

--- LeetCode/defuseTheBomb.rb
+++ LeetCode/defuseTheBomb.rb
@@ -1,22 +1,29 @@
+#Navid way:
+
 def decrypt(code)
     
     # n == code.length
     # 1 <= n <= 100
     # 1 <= code[i] <= 100
     # -(n - 1) <= k <= n - 1
-    
+
     i = 0
     # k = 3
-        while i < code.length do 
-            newCode = code.sum
-            solution.push(newCode)
-            return solution[i]
-            i += 1
-        end
-    
+
+    while i < code.length do 
+        # newCode = code.sum
+        solution.push(code.sum)
+        return solution[i]
+        i += 1
     end
-    
-    code = [5, 7, 1, 4]
-    solution = []
-    
-    puts decrypt(code)
+end
+
+code = [5, 7, 1, 4]
+solution = []
+
+puts decrypt(code)
+
+#Ruby way:
+
+#it should use some sort of map.with_index {|...|....if}
+#that's it for now :(
