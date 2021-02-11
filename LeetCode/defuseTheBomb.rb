21cd1cc8542a002173470dcec2adbfbe20866ca1
Amir Vafaei <amir.vafaei@vineti.com>
2021-02-11 23:38:34 +0400

    failed to defuse the bomb

--- LeetCode/defuseTheBomb.rb
+++ LeetCode/defuseTheBomb.rb
@@ -1,6 +1,6 @@
 #Navid way:
 
-def decrypt(code)
+def decrypt(code), key
     
     # n == code.length
     # 1 <= n <= 100
@@ -11,9 +11,8 @@ def decrypt(code)
     # k = 3
 
     while i < code.length do 
-        # newCode = code.sum
-        solution.push(code.sum)
-        return solution[i]
+        key.push(code.sum)
+        return key[i]
         i += 1
     end
 end
@@ -21,9 +20,22 @@ end
 code = [5, 7, 1, 4]
 solution = []
 
-puts decrypt(code)
+puts decrypt(code, key)
 
 #Ruby way:
 
 #it should use some sort of map.with_index {|...|....if}
 #that's it for now :(
+
+    def decrypt(code, key)
+        code = [5, 7, 1, 4]
+        key = []
+        # get 3 elements from the code
+        def code.cycle.each_slice(3).take(4).push()
+            #add them together
+            return key
+    
+            # output a new array = key
+        end
+    
+    end
