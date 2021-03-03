90848ab310550fe9224e0c0b89e48a4fc542d731
Amir Vafaei <amir.vafaei@vineti.com>
2021-03-03 22:27:20 +0400

    started working on my homeworks

--- Navid-Interview/homeWork.rb
+++ Navid-Interview/homeWork.rb
@@ -8,10 +8,33 @@ class ColorDuo
   end
   puts translate_color_to_number(x)
 end
+# ====== continued working on the color duo problem
 
-  #we need to have "list" in line 4 and another name in line 7. Make it work!
+class ResistorColorDuo
+  COLORS = %w(black brown red orange yellow grEen blue violet Grey wHite)
+  list = %w(black brown white)
+  l2 = COLORS.map.with_index {|n, i| [i]}
+  puts l2
+end
+
 
   # given an array that has a pair of duplicate, find the duplicate(try to solve it with different algotithms)
+  class ResistorColorDuo
+    COLORS = %w(black red orange red yellow grEen blue violet Grey wHite)
+    i = 0
+    n = "" #black
+    duplicate = ""
+    while i < COLORS.length do 
+      if COLORS[i] == n
+        duplicate = n
+      end
+      if COLORS[i] != n
+        n = COLORS[i]
+      end
+      i += 1
+    end
+    puts duplicate
+end
 
   # sovlve all the questions you solved with while usig for loops.
   # one leet code per day solved by next week AND show what you learned about Ruby
