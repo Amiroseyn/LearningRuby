703eada2b418beb8f9e51da1f94aeb82a1e72f8c
Amir Vafaei <amir.vafaei@vineti.com>
2021-03-15 22:46:12 +0400

    continued working on series

--- Exercism/resistor-color-duo/resistor_color_duo.rb
+++ Exercism/resistor-color-duo/resistor_color_duo.rb
@@ -12,3 +12,16 @@ class ResistorColorDuo
       list.first(2).map {|i| COLORS.index(i)}.join.to_i.freeze
     end
 end
+
+
+# =====
+# first take
+# COLORS = %w(Black Brown Red Orange Yellow Green Blue Violet Grey White)
+# def self.value(list)
+#   #get the values in the list
+#   l2 = list.first(2).map do |color|
+#       #get the index of the item
+#       COLORS.map(&:downcase).index(color)
+#   end
+#   return l2.join.to_i
+# end
--- Exercism/series/series.rb
+++ Exercism/series/series.rb
@@ -7,3 +7,20 @@ To get started with TDD, see the `README.md` file in your
 =end
 
 
+class Series
+    size = 0
+    def initialize(string)
+      @string = string
+    end
+    def slice(size)
+      @string.first()
+    end
+  end
+  
+  my_series = Series.new('32456')
+  pp my_series.slice(4)
+  
+  
+  slc_size - arr.length + 1 = slc_count
+  slc_count.times.map {|i|   }
+
