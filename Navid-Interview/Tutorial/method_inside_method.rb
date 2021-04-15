bf05aff911f4acd75bfd44ac3daccaf5c89817d4
Amir Vafaei <amir.vafaei@vineti.com>
2021-04-16 03:47:33 +0400

    added 2 solutions for sum of squeare of array

--- Navid-Interview/Tutorial/method_inside_method.rb
+++ Navid-Interview/Tutorial/method_inside_method.rb
@@ -1,4 +1,4 @@
-def sum(number, other)
+  def sum(number, other)
     number + other
   end
   
@@ -39,4 +39,60 @@ def sum(number, other)
   end
   
   array = [2, 5, 3]
+  puts transform_array(array)
+
+  # An other way of solving the problem, but I'm not sure if that's what Navid had in mind...
+
+  def transform_array(array)
+    i = 0
+    array2 = []
+    while i < array.length
+      num1 = array[i]
+      array2.push(square(num1))
+      i += 1
+    end
+    # puts array2
+    i = 0
+    sum = 0
+    while i < array2.length
+      if sum < array2[i]
+        sum += array2[i]
+      end
+      # puts sum
+    i += 1 
+    end
+    return sum
+  end
+  
+  def square(num1)
+    num1 * num1
+  end
+  
+  array = [1, 3]
+  puts transform_array(array)
+
+  # this is probably more like it
+
+  def transform_array(array)
+    i = 0
+    array2 = []
+    while i < array.length
+      num1 = array[i]
+      array2.push(square(num1))
+  
+      i += 1
+    end
+    # puts array2
+    return return_sum(array2)
+  end
+  
+  def square(num1)
+    num1 * num1
+  end
+  
+  def return_sum(array2)
+      array2.sum
+  end
+  
+  array = [2, 3]
   puts transform_array(array)
