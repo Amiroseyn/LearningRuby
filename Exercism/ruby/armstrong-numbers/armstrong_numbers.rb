72fcb8ac452b817ed70074d9c630bc3e1213c856
Amir Vafaei <amir.vafaei@vineti.com>
2021-06-28 19:45:02 +0400

    the base for the ArmstrongNumber

---
+++ Exercism/ruby/armstrong-numbers/armstrong_numbers.rb
@@ -0,0 +1,20 @@
+=begin
+Write your code for the 'Armstrong Numbers' exercise in this file. Make the tests in
+`armstrong_numbers_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/armstrong-numbers` directory.
+=end
+
+
+class ArmstrongNumbersTest
+
+    def self.include?(input)
+        thePowerNumber = input.to_s.length
+        input.each_with_index do |singleNumber|
+            singleNumber ** thePowerNumber
+        end
+    end
+
+
+end
