bc5575b8e574eaf7cf51b8a7eb0e729eb7d3725d
Amir Vafaei <amir.vafaei@vineti.com>
2021-07-07 15:40:52 +0400

    rewrote what I've learned with Ali

---
+++ Exercism/ruby/Ali-lecture/class_methods.rb
@@ -0,0 +1,69 @@
+class Person
+    LEVELS = %i(intern junior mid senior lead).freeze
+    attr_reader :first_name, :last_name, :level
+  
+  
+    def initialize(first_name, last_name, level = :mid)
+      @first_name = first_name
+      @last_name = last_name
+      @level = level
+    end
+  
+    def fullname
+      # @first_name + " " + @last_name
+      "#{@first_name} #{@last_name}"
+    end
+  
+    def promote!
+      @level = LEVELS[LEVELS.index(@level) + 1]
+    end
+  
+  end
+  
+  
+  class Team
+    def initialize(team_name, members)
+      @name = team_name
+      @members = members
+    end
+  
+    def add_member(person)
+      @members << person
+    end
+  
+    def interns
+      @members.select { |m| m.level == :intern }
+    end
+   
+    def juniors
+      
+    end
+  
+    def mids
+      @members.select { |m| m.level == :mid }
+    end
+  
+    def seniors
+      
+    end
+  
+    def leads
+  
+    end
+  end
+  
+  
+  amir = Person.new("Amir", "Vafa")
+  ali = Person.new("Ali", "Monemian")
+  hayk = Person.new("Hayk", "Shoghunts")
+  ashot = Person.new("Ashot", "Grigoryan")
+  hayk.promote!
+  team = Team.new("Cortage", [ali])
+  
+  cortage = Team.new("Cortage", [amir])
+  cortage.add_member(hayk)
+  cortage.add_member(ashot)
+  pp cortage.mids
+  
+    
+  
---
+++ Exercism/ruby/matrix/matrix.rb
@@ -0,0 +1,19 @@
+=begin
+Write your code for the 'Matrix' exercise in this file. Make the tests in
+`matrix_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/matrix` directory.
+=end
+
+class Matrix
+
+input = ("1 2\n10 20")
+
+    def self.new(input)
+        input.each do |row, columns|
+            if 
+        end
+    end
+
+end
