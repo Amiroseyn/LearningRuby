0199eec4a7cd5921ca93fdc9e161486ff2a11af5
Amir Vafaei <amir.vafaei@vineti.com>
2021-07-20 22:20:43 +0400

    added half of the Matrix solution

--- Exercism/ruby/matrix/matrix.rb
+++ Exercism/ruby/matrix/matrix.rb
@@ -8,12 +8,25 @@ To get started with TDD, see the `README.md` file in your
 
 class Matrix
 
-input = ("1 2\n10 20")
+# input = ("1 2\n10 20") #"1 2", "10, 20"
 
-    def self.new(input)
-        input.each do |row, columns|
-            if 
+    def initialize(input)
+        @res = input.split("\n").map do |str|
+            str.split(" ").map(&:to_i) #[[1, 2],[10, 20]]
+        end
+        
+    end
+
+    def rows
+      @res
+    end
+
+    def columns
+        res.map_with_index do |row, index|
+            row[index]
         end
     end
 
 end
+
+
